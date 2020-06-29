8f468649ca94f452461cde9385e4cee069ca1c50
Tomas Coox <tomas@joox.se>
2020-06-29 16:37:33 +0200

    Updates

---
+++ precache-manifest.5a50a33309c4cc164997fb644e92be27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f99c79226669942046be4e81e8e0668a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5a6018d9e72f772712a9",
+    "url": "/monsters-rolodex/static/css/main.43dd6c04.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a6018d9e72f772712a9",
+    "url": "/monsters-rolodex/static/js/main.7a65cecd.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
